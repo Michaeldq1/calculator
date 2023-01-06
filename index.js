7bb55b6ea0f6886bf958875caa6efa32773b2627
Michaeldq1 <michaeldquinton@gmail.com>
2023-01-06 07:06:32 -0600

    Comments to describe event listeners

--- index.js
+++ index.js
@@ -1,24 +1,20 @@
 "use strict";
 
-
-const display = document.getElementById('calculator-display');
-const numberKeys = document.querySelectorAll('.number-key');
-const operatorKeys = document.querySelectorAll('.operator-key');
-const returnKey = document.getElementById('return-key');
-const clearKey = document.getElementById('clear-key');
-const deleteKey = document.getElementById('delete-key');
-let isOutputDisplayed = false;
-
+//when a number key is clicked, append number to input string.
 for (const number of numberKeys) {
     number.addEventListener('click', insertNumber);
 }
 
+//when an operator key is clicked, append opartor to input string.
 for (const operator of operatorKeys) {
     operator.addEventListener('click', insertOperator);
 }
 
+//when the return key is clicked, run math operations against the input string.
 returnKey.addEventListener('click', runOperations);
 
+//when the delete key is clicked, remove last character from input string.
 deleteKey.addEventListener('click', removeLastCharacter);
 
+//when the clear key is clicked, clear the input string.
 clearKey.addEventListener('click', clearString);
